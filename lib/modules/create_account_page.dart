4a3d9aa6c6811db765d2ed7ea3baff56b684ca74
erisvaldocorreia2 <erisvaldo.correia@rethink.dev>
2021-12-17 10:20:37 -0300

    criando a base de tela para criar nova conta de usuario

--- lib/modules/create_account_page.dart
+++ lib/modules/create_account_page.dart
@@ -1,10 +1,54 @@
 import 'package:flutter/material.dart';
 
+import '/components/button_widget.dart';
+import '/components/input_widget.dart';
+import '/shared/button_type.dart';
+
 class CreateAccountPage extends StatelessWidget {
   const CreateAccountPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(40.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Crie sua conta!'),
+            Text('Mantenha suas contas em dia'),
+            InputText(
+              label: "Nome",
+              hint: "Digite seu nome completo",
+            ),
+            SizedBox(
+              height: size.height * .02,
+            ),
+            InputText(
+              label: "E-mail",
+              hint: "Digite seu E-mail",
+            ),
+            SizedBox(
+              height: size.height * .02,
+            ),
+            InputText(
+              label: "Senha",
+              hint: "Digite sua senha",
+            ),
+            SizedBox(
+              height: size.height * .07,
+            ),
+            Button(
+              label: 'Criar Conta',
+            ),
+            SizedBox(
+              height: size.height * .03,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
